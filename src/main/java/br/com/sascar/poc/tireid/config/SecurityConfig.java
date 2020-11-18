25e6c488184106e33db445b4e12fc3a6644b86f3
Allan Barreto Gaspar <allan.gaspar@meta.com.br>
2020-11-18 08:46:50 -0300

    commither

--- src/main/java/br/com/sascar/poc/tireid/config/SecurityConfig.java
+++ src/main/java/br/com/sascar/poc/tireid/config/SecurityConfig.java
@@ -30,7 +30,7 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter{
 	protected void configure(HttpSecurity http) throws Exception {
 		
 		http.authorizeRequests()
-		.antMatchers("/v2/api-docs", "/configuration/**", "/swagger*/**", "/webjars/**").permitAll()
+		.antMatchers("/poc-tireid/usuario","/v2/api-docs", "/configuration/**", "/swagger*/**", "/webjars/**").permitAll()
 		.anyRequest().authenticated()
 		.and().csrf().disable();
 		http.addFilter(new JWTAutheticationFilter(authenticationManager(), jWtUtil));
